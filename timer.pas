89884e4ed2c02374e9b50d568051fe0728f2f3a9
Ирина Григорян <iraagreg@gmail.com>
2024-10-05 05:53:00 +0300

    main:

---
+++ timer.pas
@@ -0,0 +1,5 @@
+﻿begin
+  var (h, m, s) := ReadInteger3('Введите время в часах, минутах и секундах:');
+  Assert((h in 0..23) and (m in 0..59) and (s in 0..59));
+  Print('Время в секундах:', h*3600 + m*60 + s)
+end.
