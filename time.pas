d96f336cc9eb8e88dcc7d091a2339b5a591cf69c
Ирина Григорян <iraagreg@gmail.com>
2024-10-05 05:47:35 +0300

    final

--- time.pas
+++ time.pas
@@ -1,3 +1,12 @@
 ﻿begin
-  
+  var year := ReadInteger('Введите год:');
+  var days: integer;
+  if year mod 4 = 0 then
+    if  (year mod 100 = 0) and (year mod 400 <> 0) then
+      days := 365
+    else
+      days := 366
+  else
+    days := 365;
+  Print(days);
 end.
