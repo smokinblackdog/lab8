74d7e39d442034dd803c1400bf9e7ecd8a8987cd
Ирина Григорян <iraagreg@gmail.com>
2024-10-05 05:33:11 +0300

    new

--- hello.pas
+++ hello.pas
@@ -1,3 +1,9 @@
 ﻿begin
-  Print('Hello World!');
+  var time:= ReadInteger('Который час?');
+  case time of
+    4..10: Print('Доброе утро, мир!');
+    11..16: Print('Добрый день, мир!');
+    17..22: Print('Добрый вечер, мир!');
+    else: Print('Доброй ночи, мир!');
+  end;
 end.
